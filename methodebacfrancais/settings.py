308e4d741d82e561f5bcf0c4dfad1b0ac962b688
Raistlin11123 <37593164+Raistlin11123@users.noreply.github.com>
2019-06-29 17:21:21 +0200

    'second'

--- methodebacfrancais/settings.py
+++ methodebacfrancais/settings.py
@@ -130,6 +130,12 @@ STATICFILES_DIRS = [
 
 LOGIN_REDIRECT_URL = '/mainapp/'
 
-#only for local
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# The absolute path to the directory where collectstatic will collect static files for deployment.
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
 
